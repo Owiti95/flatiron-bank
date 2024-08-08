6cff994eb58c8ec1690d8b4b292ebe5ae2c7e692
hannington owiti <hanningtonowiti@gmail.com>
2024-08-08 21:47:52 +0300

    created files

---
+++ flatiron-bank/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './Form';
+import ItemList from './ItemList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Grocery List App</h1>
+      <Form />
+      <ItemList />
+    </div>
+  );
+};
+
+export default App;
